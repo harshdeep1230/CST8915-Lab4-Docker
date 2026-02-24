b708be40fb2213d3066bcb5eb3a5d4e1b3aae49b
Ubuntu <azureuser@DockerHost.lchidxpbvsbejpnin2k2xhpagd.cx.internal.cloudapp.net>
2026-02-24 20:39:55 +0000

    Initial commit: Docker Compose Lab files

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import redis
+import os
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+@app.route('/')
+def hello():
+    count = cache.incr('hits')
+    return f'Hello! This page has been visited {count} times.\n'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
